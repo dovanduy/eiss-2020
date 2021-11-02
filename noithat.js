661de149e794b2eb1d195256c2860e4f1e35c63e
Đỗ Văn Duy <duyktpm2@gmail.com>
2021-11-02 23:42:59 +0700

    Create noithat.js
    bbb

---
+++ noithat.js
@@ -0,0 +1,5 @@
+var meta = document.createElement('meta');
+meta.setAttribute('itemprop', 'description');
+meta.setAttribute('content', 'dovanduy');
+document.getElementsByTagName('head')[0].appendChild(meta);
+console.log('xong')
